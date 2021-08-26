8932d7500282bdbc76c3806ed89741cee0a6894b
Chayan Bhowmik <imchayanbhowmik@gmail.com>
2021-08-26 21:00:15 +0530

    create a add-product & my product page

--- portal/icfai-kart/src/app/components/sign-up/sign-up.component.ts
+++ portal/icfai-kart/src/app/components/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+  onSignIn() {
+    this.router.navigate(['sign_in']);
   }
 
 }
