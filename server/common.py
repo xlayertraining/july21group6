4533f3aabd5361d8611609ec80770bcc6bfb3700
Soumik Debnath <dnsoumik.org@gmail.com>
2021-08-26 15:55:15 +0530

    Table name changed.

--- server/common.py
+++ server/common.py
@@ -27,6 +27,6 @@ if os.path.isdir(imgPath) == False:
 # Creating database connection and initializing databases and tables required for your backend
 client = motor.motor_asyncio.AsyncIOMotorClient("127.0.0.1", 27017)
 icfaiKart = client['IcfaiKart']
-users = icfaiKart['users']
-products = icfaiKart['products']
+users = icfaiKart['account']
+products = icfaiKart['product']
 search_history = icfaiKart['search_history']
