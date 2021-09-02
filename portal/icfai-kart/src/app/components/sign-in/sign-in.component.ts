0968d64329857c7c74329045e0c0c0c0c0a3d160
Nilotpal Das <nilotpaldas991@gmail.com>
2021-09-02 11:58:20 +0530

    sign-in api

--- portal/icfai-kart/src/app/components/sign-in/sign-in.component.ts
+++ portal/icfai-kart/src/app/components/sign-in/sign-in.component.ts
@@ -1,24 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NetworkService } from 'src/app/service/network.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.scss'],
+  providers: [NetworkService]
 })
 export class SignInComponent implements OnInit {
+  constructor(private networkService: NetworkService, private router: Router) { }
 
-  constructor(
-    private router: Router
-  ) { }
-
+  email: String = '';
+  password: String = '';
+  
+  
   ngOnInit(): void {
   }
 
-  
+  onSignInPost(): void {
 
-  onSignUp() {
-    this.router.navigate(['sign_up']);
+    // validation
+
+    if (this.email.length == 0) {
+      alert('Please enter your email.');
+      return;
+    }
+
+    if (this.password.length == 0) {
+      alert('Please enter your password.');
+      return;
+    }
+
+    const restBody = {
+      emailAddress: this.email,
+      password: this.password
+    };
+
+    console.log('main_body', restBody);
+
+    this.networkService.postSignIn(restBody).subscribe( success => {
+      console.log(success);
+      if (success.status == true) {
+        // sign in success
+        localStorage.setItem(environment.authKey, success.result[0].Authorization);
+        this.router.navigate(['home']);
+      } else {
+        alert(success.message);
+      }
+      
+    }, error => {
+
+    });
+
   }
 
+  onSignUp(): void {
+    this.router.navigate(['sign_up']);
+  }
 }
